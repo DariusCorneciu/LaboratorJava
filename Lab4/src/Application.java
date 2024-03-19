e303faf145a98f50a693617770efdd96e70ec423
Darius Corneciu <darius-stefan.corneciu@s.unibuc.ro>
2024-03-19 14:44:57 +0200

    Primul exercitiu

---
+++ Lab4/src/Application.java
@@ -0,0 +1,14 @@
+import CalculArii.*;
+
+public class Application {
+    public static void main(String[] args) {
+    Cerc cerc = new Cerc(10);
+    Patrulater patrat = new Patrulater(10,10);
+        System.out.println(cerc.calculPerimetru());
+        System.out.println(patrat.calculPerimetru());
+
+
+
+
+    }
+}
